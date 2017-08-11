93db7ca66f1a33d712942156a5d9df1a1fb2fc5e
FujiHaruka <fujiharuka@realglobe.jp>
2017-08-11 22:51:15 +0900

    Imple escape \$FOO

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 extern crate regex;
-
+extern crate uuid;
 use std::io::{self, Read};
 use std::env;
-// use std::fmt;
+use uuid::Uuid;
 use regex::Regex;
+use std::collections::HashSet;
+use std::iter::FromIterator;
 
 fn main() {
     let mut input = String::new();
@@ -12,10 +14,11 @@ fn main() {
     let var_re = Regex::new(r"\$([[:word:]]+)").unwrap();
 
     let mut output = input.clone();
-    for caps in var_re.captures_iter(&input) {
-        let var_name = &caps[1];
-        let var_value = &value_from_env(var_name);
-        output = replace_var(&output, (var_name, var_value));
+
+    let var_names = get_subst_names(&input, var_re);
+    for var_name in var_names.into_iter() {
+        let var_value = &value_from_env(&var_name);
+        output = replace_var(&output, (&var_name, var_value));
     }
     println!("{}", output);
 }
@@ -27,10 +30,21 @@ fn value_from_env(var_name: &str) -> String {
     }
 }
 
-fn replace_var(target: &str, var: (&str, &str)) -> String {
+fn replace_var(text: &str, var: (&str, &str)) -> String {
     let var_name = var.0;
     let var_value = var.1;
-    target.replace(&format!("${}", var_name), var_value)
-          // `{` character is escaped with `{{`
+    let tmp_evac1: &str = &Uuid::new_v4().simple().to_string();
+    let tmp_evac2: &str = &Uuid::new_v4().simple().to_string();
+    text.replace(&format!(r"\${}", var_name), tmp_evac1)
+          .replace(&format!(r"\${{{}}}", var_name), tmp_evac2)
+          .replace(&format!("${}", var_name), var_value)
           .replace(&format!("${{{}}}", var_name), var_value)
+          .replace(tmp_evac1, &format!("${}", var_name))
+          .replace(tmp_evac2, &format!("${{{}}}", var_name))
+}
+
+fn get_subst_names(text: &str, var_re: Regex) -> HashSet<String> {
+    let var_names_iter = var_re.captures_iter(text).map(|cap| cap[1].to_string());
+    let var_names: HashSet<String> = HashSet::from_iter(var_names_iter);
+    var_names
 }
