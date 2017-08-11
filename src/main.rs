4227bbc6b985f0401b16835fa764709619f0ceb5
FujiHaruka <fujiharuka@realglobe.jp>
2017-08-11 16:32:06 +0900

    Read from stdin and println

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    println!("{}", input);
 }
