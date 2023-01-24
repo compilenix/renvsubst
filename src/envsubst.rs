3086fc22043964193c4ac4bb77f8af07e90bffb5
CompileNix <compilenix@gmail.com>
2023-01-24 05:05:33 +0100

    Add some more tests

--- src/envsubst.rs
+++ src/envsubst.rs
@@ -9,7 +9,6 @@ pub fn replace(input: String) -> String {
     let var_pattern = Regex::new(r"(\\*)\$(?P<head>\{([[:word:]]+)\}|([[:word:]]+))").unwrap();
 
     let output = var_pattern.replace_all(&input, |caps: &regex::Captures| {
-
         // grap any \ chars at the beginning of the match, which would otherwise be thrown away in the output
         let mut pre_escape_chars = "";
         if caps.get(1).map_or(false, |m| m.as_str().len() % 2 == 1) {
@@ -75,6 +74,52 @@ mod tests {
 
         let input = r#"Hello, $FOO and ${BAR}, $NON_EXISTENT"#.to_string();
         let expected_output = r#"Hello, bar and baz, $NON_EXISTENT"#;
+        let output = replace(input);
+        assert_eq!(output, expected_output);
+    }
+
+    #[test]
+    fn replace_multiple_times() {
+        env::set_var("FOO", "bar");
+        env::set_var("BAR", "baz");
+
+        // Test multiple instances of the same variable
+        let input = r#"$FOO $FOO $FOO"#.to_string();
+        let expected_output = r#"bar bar bar"#;
+        let output = replace(input);
+        assert_eq!(output, expected_output);
+    }
+
+    #[test]
+    fn replace_numbers_and_special_chars() {
+        env::set_var("NUM", "123");
+        env::set_var("SPECIAL", r#"!@#"#);
+
+        // Test with numbers and special characters
+        let input = r#"Number: $NUM, Special: $SPECIAL"#.to_string();
+        let expected_output = r#"Number: 123, Special: !@#"#;
+        let output = replace(input);
+        assert_eq!(output, expected_output);
+    }
+
+    #[test]
+    fn replace_unicode_emoji() {
+        env::set_var("EMOJI", "😳");
+
+        // Test with numbers and special characters
+        let input = "Emoji: $EMOJI".to_string();
+        let expected_output = "Emoji: 😳";
+        let output = replace(input);
+        assert_eq!(output, expected_output);
+    }
+
+    #[test]
+    fn replace_empty() {
+        env::set_var("EMPTY", "");
+
+        // Test with empty variable
+        let input = "Empty var: $EMPTY".to_string();
+        let expected_output = "Empty var: ";
         let output = replace(input);
         assert_eq!(output, expected_output);
     }
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,9 @@ mod envsubst;
 
 use std::io::{self, Read};
 
-
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
     let output = envsubst::replace(input);
     print!("{output}");
 }
-
